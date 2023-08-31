4ffed73324210cd94329b9e970d9d03d8d3f5633
Jonathan Mejia <jefry.jonathan.quintanilla@gmail.com>
2023-08-31 11:43:29 -0600

    Update Proyect_card.jsx

--- components/Proyect_card.jsx
+++ components/Proyect_card.jsx
@@ -14,7 +14,7 @@ const ProyectCard = ({ titulo, descripcion, etiquetas, IMG, reftoproyectlink, re
             <div className="lg:w-1/4 px-2 flex lg:items-center justify-center"> {IMG}  </div>
             <div className='lg:w-3/4'>
                 <h3 className='text-lg py-1/100 flex items-center text-dark font-bold'>{titulo} 
-                <a href={reftoproyectlink}><FaLink className=' text-md mx-2 hover:text-blue-500 transition cursor-pointer duration-500'/></a> 
+                <a href={reftoproyectlink}><FaLink className=' text-md mx-2 hover:text-gray-500 transition cursor-pointer duration-500'/></a> 
                 <a href={refttoproyect}> <FaGithub className=' text-xl mx-1 hover:text-black transition cursor-pointer duration-500'/></a> </h3>
                 <p className='text-sm text-primary mt-2 text-justify' >{descripcion}</p>
                 <ul className='flex  w-full mt-3 flex-wrap '>
