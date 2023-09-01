5c8f64abeef4d94fced1e1aa01ca7b49a96a898f
Jonathan Mejia <jefry.jonathan.quintanilla@gmail.com>
2023-09-01 12:24:28 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,17 +3,16 @@ import { useState } from 'react';
 import Image from 'next/image';
 import About_me_section from '../components/about_me_section';
 import Main_index_section from '../components/main_index_section';
+import { FaAnchor } from 'react-icons/fa';
 
 export default function Home() {
   const [animationFinished, setAnimationFinished] = useState(false);
-
   const onAnimationFinish = () => {
     setAnimationFinished(prevState => !prevState);
   };
-  
-
   return (
     <main className="flex flex-col items-center justify-between bg-neutral ">
+  
       {animationFinished ? (
         <Main_index_section reverseAnimation={onAnimationFinish} />
       ) : (
